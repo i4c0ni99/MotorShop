0eb9268d6d701f9dd7cd1ed46a662002f0022c41
i4c0ni99 <i4c0ni99@gmail.com>
2022-09-29 15:45:51 +0200

    sessione funzionante ma da ritoccare

--- dashBoard.php
+++ dashBoard.php
@@ -2,10 +2,7 @@
 require "include/template2.inc.php";
 require "include/dbms.inc.php";
 
-foreach($_SESSION['user'] as $item){
-    echo $item['name'];
-} 
  $main = new Template("skins/multikart_all_in_one/back-end/index.html");
- 
+$main->setContent('user',$_SESSION['user']['name']);
  $main->close();
 ?>
