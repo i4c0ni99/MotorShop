29c9dccac10cfe89a890ef9f0747451f89613bd6
longobucco <luca.visconti2@student.univaq.it>
2022-10-19 20:11:32 +0200

    Validation 2

---
+++ reset-password.php
@@ -0,0 +1,13 @@
+<?php 
+
+require "include/template2.inc.php";
+
+$main = new Template("skins/motor-html-package/motor/frame_public.html");
+$body = new Template("skins/motor-html-package/motor/forgot-password.html");
+
+$main->setContent("dynamic", $body->get());
+
+$main->close();
+
+
+?>
