9b04755976a3868620e60870cc7be313c4f2d244
i4c0ni99 <64600662+i4c0ni99@users.noreply.github.com>
2024-10-26 11:42:35 +0200

    fix usr list (#26)
    * fix usr list
    
    * [..]

--- skins/multikart_all_in_one/back-end/assets/js/checkbox-all-check.js
+++ skins/multikart_all_in_one/back-end/assets/js/checkbox-all-check.js
@@ -11,3 +11,50 @@ $("#checkall").change(function () {
         });
     }
 });
+$(document).ready(function() {
+    // Listener per ciascuna checkbox
+    $(".check-it").change(function() {
+        var email = $(this).data("email"); // Recupera l'email associata
+
+        if ($(this).is(":checked")) {
+            console.log("Checkbox per l'email " + email + " è selezionata.");
+            // Esegui qui la tua chiamata AJAX, se necessario
+            $.ajax({
+                url: "user-list.php",
+                method: "POST",
+                data: {
+                    selected_user: email,
+                    change_role: 1 // Indica l'azione di selezione
+                },
+                success: function(response) {
+                    console.log("Ruolo aggiornato per: " + email);
+                    window.location.href="user-list.php"
+                },
+                error: function() {
+                    alert("Errore durante la connessione al server.");
+                }
+            });
+        } else {
+            console.log("Checkbox per l'email " + email + " è deselezionata.");
+            // Chiamata AJAX per deselezionare, se necessario
+            $.ajax({
+                url: "user-list.php",
+                method: "POST",
+                data: {
+                    selected_user: email,
+                    change_role: 2 // Indica l'azione di deselezione
+                },
+                success: function(response) {
+                    console.log("Ruolo rimosso per: " + email);
+                    window.location.href="user-list.php"
+                },
+                error: function() {
+                    alert("Errore durante la connessione al server.");
+                }
+            });
+        }
+    });
+
+    // Opzione per selezionare/deselezionare tutte le checkbox
+    
+});
